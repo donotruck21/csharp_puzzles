68dbf3e2b8a4231788c337ed188c1cbb070008e0
Donovan An <donovantaean@yahoo.com>
2017-01-11 10:49:14 -0800

    finished random array challenge

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace ConsoleApplication
 {
     public class Program
     {
+        public static int[] RandomArray(){
+            int[] arr = new int[10];
+            Random r = new Random();
+            for(int i = 0; i < arr.Length; i++){
+                // int rInt = r.Next(5, 25);
+                arr[i] = r.Next(5, 25);
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+            return arr;
+        }
+
+
+
+
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            RandomArray();
         }
     }
 }
